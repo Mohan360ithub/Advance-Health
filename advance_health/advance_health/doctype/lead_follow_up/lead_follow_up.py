a74b5af4c17d124c06f970e5dde252f1ce7e89ef
Frappeuser <frappeuser@ip-172-31-22-254.ap-south-1.compute.internal>
2024-03-13 09:04:27 +0000

    form live

---
+++ advance_health/advance_health/doctype/lead_follow_up/lead_follow_up.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2024, pankaj@360ithub.com and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+class LeadFollowUp(Document):
+    def before_insert(self):
+        lead_id = self.lead_id
+        lead = frappe.get_doc("Lead", lead_id)
+
+        if lead and lead.followed_up != 1:
+            lead.followed_up = 1
+            lead.save(ignore_permissions=True)  # Update the lead's followed_up field
+            #frappe.msgprint(_("Lead {0} marked as followed up.").format(lead_id))
+
